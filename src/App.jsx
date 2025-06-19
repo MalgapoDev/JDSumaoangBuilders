06341fb0039fa97279c391a4b2946a1caaf75fd0
Michael <malgapomichaelmarc@gmail.com>
2025-06-20 00:21:32 +0800

    import new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
-
-import NavigationBar from './components/NavigationBar'
-import Heropage from './pages/Heropage'
-import About from './pages/About'
-import Service from './pages/Service'
-import Projects from './pages/Projects'
-import Contacts from './pages/Contacts'
-import Footer from './components/Footer'
+import NavigationBar from './components/NavigationBar';
+import Heropage from './pages/Heropage';
+import About from './pages/About';
+import Service from './pages/Service';
+import Projects from './pages/Projects';
+import Contacts from './pages/Contacts';
+import Footer from './components/Footer';
+import NotFound from './pages/NotFound';
+import ContactForm from './pages/ContactForm';
 
 function App() {
-
   useEffect(() => {
-        // Initialize AOS
-        Aos.init({
-            duration: 2000,
-            offset: 50,
-            startEvent: 'DOMContentLoaded', // Helps with initial render
-        });
+    Aos.init({
+      duration: 2000,
+      offset: 50,
+    });
 
-        // Make AOS available globally for debugging
-        window.Aos = Aos;
-    }, []);
+    window.Aos = Aos;
+  }, []);
 
   return (
-    <div className="App">
-      <header>
-        <NavigationBar />
-      </header>
-      <div id="home">
-        <Heropage />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="services">
-        <Service />
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-      <div id="contacts">
-        <Contacts />
-      </div>
-      <Footer />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <NavigationBar />
+              <Heropage />
+              <About />
+              <Service />
+              <Projects />
+              <Contacts />
+              <Footer />
+            </>
+          } />
 
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/get-in-touch" element={<ContactForm />} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
